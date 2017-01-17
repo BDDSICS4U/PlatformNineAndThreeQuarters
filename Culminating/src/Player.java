37d3203628771a613ea081b62177bb9c5fa847e6
djnuc <djnuc@MSI>
2017-01-17 14:02:03 -0500

    started adding fields to player

--- Culminating/src/Player.java
+++ Culminating/src/Player.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Player extends GamePiece {
+	private int hp, coins;
+	private boolean displayed;
 	private PlayerType shape;
 	private BufferedImage image1;
 	private BufferedImage image2;
@@ -13,6 +15,8 @@ public class Player extends GamePiece {
 	public Player(double x, double y, int left, int right, int top, int bottom, PlayerType shape) {
 		super(x, y, left, right, top, bottom);
 		this.shape = shape;
+		setHP(3);
+		this.coins = 0;
 		try {
 			image1 = ImageIO.read(new File("src/resources/Smiley.svg.png"));
 			image2 = ImageIO.read(new File("src/resources/Smiley.svg.png"));
@@ -27,12 +31,14 @@ public class Player extends GamePiece {
 		case PLAYER1: {
 			System.out.println("asdf");
 			image.getGraphics().drawImage(image1, (int) getX(), (int) getY(), 30, 30, null);
-			//g.drawImage(image1.getScaledInstance(20, 20, Image.SCALE_DEFAULT), (int) getX(), (int) getY(), null);
+			// g.drawImage(image1.getScaledInstance(20, 20,
+			// Image.SCALE_DEFAULT), (int) getX(), (int) getY(), null);
 			break;
 		}
 		case PLAYER2: {
 			image.getGraphics().drawImage(image2, (int) getX(), (int) getY(), 30, 30, null);
-			//g.drawImage(image2.getScaledInstance(20, 20, Image.SCALE_DEFAULT), (int) getX(), (int) getY(), null);
+			// g.drawImage(image2.getScaledInstance(20, 20,
+			// Image.SCALE_DEFAULT), (int) getX(), (int) getY(), null);
 			break;
 		}
 		}
@@ -41,6 +47,18 @@ public class Player extends GamePiece {
 
 	public void animateOneStep() {
 		this.draw(image1);
+	}
+
+	public void setHP(int hp) {
+		this.hp = hp;
+	}
+
+	public int getHP() {
+		return this.hp;
+	}
+	
+	public void setCoins(int coin){
+		this.coins = coin;
 	}
 
 }
